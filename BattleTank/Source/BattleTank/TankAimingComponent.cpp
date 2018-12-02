b7527617b62deb790b08cec86670b7525c70596a
DESKTOP-1L84BIH\Ryan <RyanC.Barr@gmail.com>
2018-12-02 13:14:05 -0800

    Section 4, lesson 143 complete

--- BattleTank/Source/BattleTank/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/TankAimingComponent.cpp
@@ -14,30 +14,50 @@ UTankAimingComponent::UTankAimingComponent()
 }
 
 
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LaunchSpeed)
+	if (!Barrel) { return; }
+	FVector outLaunchVelocity(0);
+	FVector startLocation = Barrel->GetSocketLocation(FName("Projectile"));
+
+	//calculate outlaunch velocity	
+	bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity
+	(
+		this,
+		outLaunchVelocity,
+		startLocation,
+		HitLocation,
+		LaunchSpeed,
+		ESuggestProjVelocityTraceOption::DoNotTrace
+	);
+
+	if (bHaveAimSolution)
+	{
+		auto aimDirection = outLaunchVelocity.GetSafeNormal();
+
+		MoveBarrelTowards(aimDirection);
+
+		//old logging stuff
+		//auto tankName = GetOwner()->GetName();
+		//UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *tankName, *aimDirection.ToString())
+	}
 }
 
 void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
 {
 	Barrel = BarrelToSet;
+}
+
+void UTankAimingComponent::MoveBarrelTowards(FVector aimDirection)
+{
+	//rotate turret to align socket with correct firing solution location
+
+	//get difference between current barrel rotation and aimDirection
+	auto barrelRotator = Barrel->GetForwardVector().Rotation();
+	auto aimAsRotator = aimDirection.Rotation();
+	auto deltaRotator = aimAsRotator - barrelRotator;
+
+	UE_LOG(LogTemp, Warning, TEXT("AimAsRoator: %s"), *deltaRotator.ToString())
+	//move barrel the right amount this frame given max speed and frametime
+		//elevate barrel to align socket with correct firing solution location
 }
